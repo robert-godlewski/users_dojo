df3389928c1c2015a8b3a7dfd1b5ee71d0fd5ff9
Robert Godlewski <robtechg5@gmail.com>
2022-02-08 21:58:29 -0800

    able to create and see all users in website.

--- server.py
+++ server.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template, redirect, session, request
+from flask import Flask, render_template, redirect, request
+from user import User
 
 
 app = Flask(__name__)
 
 app.secret_key = 'hello users'
 
+# Main page that shows all of the users
 @app.route('/')
-def read(): return render_template("index.html")
+def read(): 
+    users = User.get_all()
+    print(users)
+    return render_template("index.html", all_users=users)
 
+# Page that allows the client to make a new user
 @app.route('/create')
 def create(): return render_template("create.html")
+
+# Creates the new user from create
+@app.route('/add_new_user', methods=['POST'])
+def add_new_user():
+    new_user = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email']
+    }
+    User.save(new_user)
+    return redirect('/')
 
 
 if __name__ == "__main__": app.run(debug=True, port=8000)
